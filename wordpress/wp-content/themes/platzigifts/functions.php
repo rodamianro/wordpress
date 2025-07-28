a2084d62c8a288e0590c59294d347a5b22196fc8
Robinson <68615462+rodamianro@users.noreply.github.com>
2025-07-28 04:32:39 +0000

    fix(style): update tailwind.css file folder

--- wordpress/wp-content/themes/platzigifts/functions.php
+++ wordpress/wp-content/themes/platzigifts/functions.php
@@ -9,7 +9,7 @@ add_action('after_setup_theme', 'init_template');
 
 function assets()
 {
-    wp_register_style('tailwind', get_template_directory_uri() . '/css/tailwind.css', array(), '1.0.0', 'all');
+    wp_register_style('tailwind', get_template_directory_uri() . '/assets/css/tailwind.css', array(), '1.0.0', 'all');
     wp_register_style('font-monserrat', 'https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap', array(), '1.0.0', 'all');
 
     wp_enqueue_style('style', get_stylesheet_uri(), array('tailwind', 'font-monserrat'), '1.0.0', 'all');
