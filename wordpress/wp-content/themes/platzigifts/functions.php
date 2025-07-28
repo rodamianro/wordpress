9f64284653e3e2fe232925673f83cf2f12f4097e
Robinson <68615462+rodamianro@users.noreply.github.com>
2025-07-28 04:23:39 +0000

    feat(style): add tailwind.css and font-monserrat to the theme

--- wordpress/wp-content/themes/platzigifts/functions.php
+++ wordpress/wp-content/themes/platzigifts/functions.php
@@ -1,7 +1,18 @@
 <?php
-function init_template(){
+function init_template()
+{
     add_theme_support('post-thumbnails');
     add_theme_support('title-tag');
 }
 
 add_action('after_setup_theme', 'init_template');
+
+function assets()
+{
+    wp_register_style('tailwind', get_template_directory_uri() . '/css/tailwind.css', array(), '1.0.0', 'all');
+    wp_register_style('font-monserrat', 'https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap', array(), '1.0.0', 'all');
+
+    wp_enqueue_style('style', get_stylesheet_uri(), array('tailwind', 'font-monserrat'), '1.0.0', 'all');
+}
+
+add_action('wp_enqueue_scripts', 'assets');
