444fa10c92df0c8e2af1a23ca5a4a4238543aa96
Robinson <68615462+rodamianro@users.noreply.github.com>
2025-07-31 18:51:22 +0000

    feat: add 404 page basic view

---
+++ wordpress/wp-content/themes/platzigifts/404.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+<main class="container px-8 py-5">
+    <div class="">
+        <h1>404 Page not found!</h1>
+        <h2><a href="<?php echo home_url() ?>">Click here</a> to go back to home</h2>
+    </div>
+</main>
+<?php get_footer(); ?>
