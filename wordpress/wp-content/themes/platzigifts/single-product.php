a4e5a699b43f82a04caf5c1f635d6f6566d4c0b9
Robinson <68615462+rodamianro@users.noreply.github.com>
2025-08-03 16:41:56 +0000

    feat: add view for product post type

---
+++ wordpress/wp-content/themes/platzigifts/single-product.php
@@ -0,0 +1,19 @@
+<?php get_header(); ?>
+
+<main class="container p-4">
+    <?php if (have_posts()): ?>
+        <?php while (have_posts()): the_post(); ?>
+            <h1 class="mt-5 text-4xl"> <?php the_title(); ?></h1>
+            <div class="flex flex-row gap-4 justify-center">
+                <div>
+                    <?php the_post_thumbnail('large'); ?>
+                </div>
+                <div>
+                    <?php the_content(); ?>
+                </div>
+            </div>
+        <?php endwhile; ?>
+    <?php endif; ?>
+</main>
+
+<?php get_footer(); ?>
