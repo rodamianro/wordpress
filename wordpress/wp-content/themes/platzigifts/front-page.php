cf19aced282e08ee4e676d63087011c452d7572b
Robinson <68615462+rodamianro@users.noreply.github.com>
2025-08-15 03:11:31 +0000

    feat: add term select

--- wordpress/wp-content/themes/platzigifts/front-page.php
+++ wordpress/wp-content/themes/platzigifts/front-page.php
@@ -14,22 +14,39 @@
         $args = array('post_type' => 'product', 'posts_per_page' => -1, 'orderby' => 'title', 'order' => "ASC");
         $products = new WP_Query($args);
         ?>
-        <?php if ($products->have_posts()): ?>
-            <div class="grid grid-cols-3 gap-4">
-                <?php while ($products->have_posts()): $products->the_post(); ?>
-                    <a href="<?php the_permalink(); ?>">
-                        <div class="border p-4 rounded">
-                            <h3 class="text-xl"><?php the_title(); ?></h3>
-                            <?php if (has_post_thumbnail()): ?>
-                                <div class="mb-4">
-                                    <?php the_post_thumbnail('medium'); ?>
-                                </div>
-                            <?php endif; ?>
-                        </div>
-                    </a>
-                <?php endwhile; ?>
+        <div class="grid grid-cols-2 gap-4">
+            <div class="px-8 mt-8">
+                <select name="products-categories" id="products-categories">
+                    <option value="">Selecciona una categoría</option>
+                    <?php $terms = get_terms(array(
+                        'taxonomy' => 'product_category',
+                        'hide_empty' => true,
+                    )) ?>
+                    <?php echo $terms ?>
+                    <?php foreach ($terms as $term): ?>
+                        <option value="<?php echo ($term->slug);  ?>">
+                            <?php echo ($term->name); ?>
+                        </option>
+                    <?php endforeach; ?>
+                </select>
             </div>
-        <?php endif; ?>
+            <?php if ($products->have_posts()): ?>
+                <div class="grid grid-cols-3 gap-4" id="products-list">
+                    <?php while ($products->have_posts()): $products->the_post(); ?>
+                        <a href="<?php the_permalink(); ?>">
+                            <div class="border p-4 rounded">
+                                <h3 class="text-xl"><?php the_title(); ?></h3>
+                                <?php if (has_post_thumbnail()): ?>
+                                    <div class="mb-4">
+                                        <?php the_post_thumbnail('medium'); ?>
+                                    </div>
+                                <?php endif; ?>
+                            </div>
+                        </a>
+                    <?php endwhile; ?>
+                </div>
+            <?php endif; ?>
+        </div>
     </div>
 </main>
 <?php get_footer(); ?>
